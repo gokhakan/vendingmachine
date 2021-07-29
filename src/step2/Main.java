68fa591c86350ae394dc5e9c4943b0475c420315
Hakan Gok <hakangok1974@yahoo.com>
2021-07-29 21:05:33 +0100

    Initial commit

---
+++ src/step2/Main.java
@@ -0,0 +1,26 @@
+package step2;
+
+import step3.TextVendingMachineInterface;
+import step2.VendingMachineInterface;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        VendingMachineInterface machineInterface = null;
+        machineInterface.displayProducts();
+
+        String selectedProduct = scanner.nextLine();
+        machineInterface.selectProduct (Integer.parseInt(selectedProduct));
+
+        machineInterface.displayEnterCoinsMessage();
+
+        String userEnteredCoins = scanner.nextLine();
+
+        int[] enteredCoins = {};
+        machineInterface.enterCoins(enteredCoins);
+        machineInterface.displayChangeMessage();
+    }
+}
